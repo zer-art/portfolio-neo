52dc8daeb21cbeb6f6e2f106ef82df2664c4dc9d
Pawan Parida <135092407+zer-art@users.noreply.github.com>
2025-12-17 11:31:05 +0530

    updated errors

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ErrorBoundary from './components/ErrorBoundary.jsx'
 
 console.log('App mounting...')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 )
